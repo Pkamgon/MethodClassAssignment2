90786150c71108927af6e202d51d51cb92b11765
Pkamgon <Pkamgon@gmail.com>
2024-01-02 21:30:48 -0700

    Add project files.

---
+++ MethodClassAssignment2/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment2
+{
+    public class Operation
+    {
+        //Create a class. In that class, create a void method that takes two integers as parameters.
+        //Have the method do a math operation on the first integer and display the second integer to the screen.
+
+        public  Operation() 
+        {
+            int num1 = 4;
+            int result = num1 * 2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Operation class
+            Operation operation = new Operation();
+            //Ask user to enter an integer
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //user will now enter value displayed above(num2)
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num2: ");
+            
+        }
+    }
+}
